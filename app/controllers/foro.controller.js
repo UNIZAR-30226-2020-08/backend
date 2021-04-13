28eda4f88721277a953ec082c354b2e970c57d8a
Misael-Arauzo <75611395+Misael-Arauzo@users.noreply.github.com>
2021-04-13 03:56:08 +0200

    foro
     "message": "insert or update on table \"foro\" violates foreign key constraint \"foro_partida_fkey\""                  Me sale todo el rato en todos un error parecido a este y la vdd que no entiendo por qué. He cambiado al principio todo por foro, luego basandome en usuario que pone tmbn username, he puesto yo partida y n_partida y nada nose que más hacer

--- app/controllers/foro.controller.js
+++ app/controllers/foro.controller.js
@@ -7,30 +7,109 @@ exports.create = (req, res) => {
       partida: req.body.partida,
       mensaje: req.body.mensaje,
     };
-    res.send({message : "Usario creado", foro});
+    Foro.create(foro)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({ message: err.message || "Error creando foro" });
+      });
   };
  
 exports.findAll = (req, res) => {
-    const foro = req.body.foro;
-    res.send({message : "Se ha encontrado el foro", foro});
+    const n_partida = req.body.partida;
+    var condition = n_partida ? { n_partida: { [Op.iLike]: `%${n_partida}%` } } : null;
+    Foro.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Error recuperando foros."
+        });
+      });
+
   };
+    
 
 exports.find = (req, res) => {
-    const foro = req.body.foro;
-    res.send({message : "Se ha encontrado el foro ", foro});
+    const partida = req.body.partida;
+    Foro.findByPk(partida)
+    .then(data => {
+        res.send({data});
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 
+                err.message || "Error recuperando foro: " + partida
+        });
+    });
   };
 
+    
 exports.update = (req, res) => {
-    const foro = req.body.foro;
-    res.send({message : "Se ha actualizado el foro ", foro});
+    const n_partida = req.body.partida;
+    Foro.update(req.body, {
+      where: { partida: n_partida }
+  })
+  .then(num => {
+      if (num == 1) {
+          res.send({
+              message: "Foro actualizado."
+          });
+      } else {
+          res.send({
+              message: `No se puede actualizar el foro : ${n_partida}.`
+          });
+      }
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: 
+              err.message || "Error actualizando foro : " + n_partida
+      });
+  });
 };
+    
 
 exports.delete = (req, res) => {
-    const foro = req.body.foro;
-    res.send({message : "Se ha eliminado el foro ", foro});
+    const n_partida = req.body.partida;
+    Foro.destroy({
+      where: { partida: n_partida }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                status: "Eliminado"
+            });
+        } else {
+            res.send({
+                status:  `No se puede eliminar el foro: ${n_partida}.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 
+                err.message || "Error eliminando el usuario con id: " + n_partida
+        });
+    });
 };
+    
 
 exports.deleteAll = (req, res) => {
-  res.send({message : "Se han eliminado todos los foros"});
-
-  };
+  Foro.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Foros eliminados.` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error eliminando foros."
+      });
+    });
+};
