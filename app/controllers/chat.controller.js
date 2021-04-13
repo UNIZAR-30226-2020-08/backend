7dea0601f651cc97c1396f4f053c568c9123c8ac
Misael-Arauzo <75611395+Misael-Arauzo@users.noreply.github.com>
2021-04-14 00:24:15 +0200

    CHAT BIEN

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -3,102 +3,69 @@ const Chat = db.chat;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
-    const chat = {
-      idchat: req.body.idchat,
-      mensaje: req.body.mensaje,
-      usuario: req.body.usuario,
-    };
-    Chat.create(chat)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Error creando chat"
-        });
-      });
+  const chat = {
+    idchat: req.body.idchat ? req.body.idchat : Math.random().toString(9).substring(2,7),
+    mensaje: req.body.mensaje,
+    usuario: req.body.usuario,
   };
- 
-exports.findAll = (req, res) => {
-    const chat = req.body.chat;
-    
-    var condition = chat ? { chat: { [Op.iLike]: `%${chat}%` } } : null;
-    Chat.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Error recuperando chats."
-        });
-      });
-  };
-
-exports.find = (req, res) => {
-    const idchat = req.body.chat;
-
-    Chat.findByPk(chat)
+  Chat.create(chat)
     .then(data => {
-        res.send({data});
+      res.send(data);
     })
     .catch(err => {
-        res.status(500).send({
-            message: 
-                err.message || "Error recuperando chat con id: " + chat
-        });
+      res.status(500).send({
+        message:
+          err.message || "Error creando chat"
+      });
     });
-  };
+};
 
-exports.update = (req, res) => {
-    const chat = req.body.chat;
-    Chat.update(req.body, {
-      where: { idchat: chat }
-  })
-  .then(num => {
-      if (num == 1) {
-          res.send({
-              message: "chat actualizado."
-          });
-      } else {
-          res.send({
-              message: `No se puede actualizar el chat con id: ${chat}.`
-          });
-      }
+exports.find = (req, res) => {
+  const idchat = req.body.idchat;
+  Chat.findByPk(idchat)
+  .then(data => {
+      res.send({data});
   })
   .catch(err => {
       res.status(500).send({
           message: 
-              err.message || "Error actualizando chat con id: " + chat
+              err.message || `Error recuperando chat con id ${idchat}`
+      });
+  });
+};
+
+exports.update = (req, res) => {
+  const idchat = req.body.idchat;
+  Chat.update(req.body, {
+    where: { idchat: idchat }
+  })
+  .then(num => {
+          res.send({ message: "chat actualizado." });
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: 
+              err.message || `Error actualizando el chat ${idchat}`
       });
   });
 };
 
 exports.delete = (req, res) => {
-    const chat = req.body.chat;
-    Chat.destroy({
-      where: { idchat: chat }
-    })
-    .then(num => {
-        if (num == 1) {
-            res.send({
-                status: "Eliminado"
-            });
-        } else {
-            res.send({
-                status:  `No se puede eliminar el chat con id: ${chat}.`
-            });
-        }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: 
-                err.message || "Error eliminando el chat con id: " + chat
-        });
-    });
+  const idchat = req.body.idchat;
+  Chat.destroy({
+    where: { idchat: idchat }
+  })
+  .then(num => {
+          res.send({ status: "Eliminado" });
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: err.message || `Error eliminando el chat ${idchat}`
+      });
+  });
 };
 
+//Sin probar por si acaso
 exports.deleteAll = (req, res) => {
   Chat.destroy({
     where: {},
@@ -114,3 +81,19 @@ exports.deleteAll = (req, res) => {
       });
     });
 };
+
+exports.findAll = (req, res) => {
+  Chat.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error recuperando chats."
+      });
+    });
+};
+
+
+
