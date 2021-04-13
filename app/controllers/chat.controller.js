ca1c681c3093ba3f9966704a7e470537b9390668
Misael-Arauzo <75611395+Misael-Arauzo@users.noreply.github.com>
2021-04-13 14:55:07 +0200

    chat
    mismos errores que todos los anteriores

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -8,30 +8,109 @@ exports.create = (req, res) => {
       mensaje: req.body.mensaje,
       usuario: req.body.usuario,
     };
-    res.send({message : "Usario creado", chat});
+    Chat.create(chat)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Error creando chat"
+        });
+      });
   };
  
 exports.findAll = (req, res) => {
     const chat = req.body.chat;
-    res.send({message : "Se ha encontrado el chat", chat});
+    
+    var condition = chat ? { chat: { [Op.iLike]: `%${chat}%` } } : null;
+    Chat.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Error recuperando chats."
+        });
+      });
   };
 
 exports.find = (req, res) => {
-    const chat = req.body.chat;
-    res.send({message : "Se ha encontrado el chat ", chat});
+    const idchat = req.body.chat;
+
+    Chat.findByPk(chat)
+    .then(data => {
+        res.send({data});
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 
+                err.message || "Error recuperando chat con id: " + chat
+        });
+    });
   };
 
 exports.update = (req, res) => {
     const chat = req.body.chat;
-    res.send({message : "Se ha actualizado el chat ", chat});
+    Chat.update(req.body, {
+      where: { idchat: chat }
+  })
+  .then(num => {
+      if (num == 1) {
+          res.send({
+              message: "chat actualizado."
+          });
+      } else {
+          res.send({
+              message: `No se puede actualizar el chat con id: ${chat}.`
+          });
+      }
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: 
+              err.message || "Error actualizando chat con id: " + chat
+      });
+  });
 };
 
 exports.delete = (req, res) => {
     const chat = req.body.chat;
-    res.send({message : "Se ha eliminado el chat ", chat});
+    Chat.destroy({
+      where: { idchat: chat }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                status: "Eliminado"
+            });
+        } else {
+            res.send({
+                status:  `No se puede eliminar el chat con id: ${chat}.`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: 
+                err.message || "Error eliminando el chat con id: " + chat
+        });
+    });
 };
 
 exports.deleteAll = (req, res) => {
-  res.send({message : "Se han eliminado todos los chats"});
-
-  };
+  Chat.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} chats eliminados.` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error eliminando chats."
+      });
+    });
+};
--- app/routes/chat.routes.js
+++ app/routes/chat.routes.js
@@ -7,7 +7,11 @@ module.exports = app => {
 
     router.get("/findChat", chat.find);
 
+    router.get("/findAllChats", chat.findAll);
+
     router.delete("/dropChat", chat.delete);
+
+    router.delete("/dropAllChats", chat.deleteAll);
   
     router.put("/updateChat", chat.update);
   
