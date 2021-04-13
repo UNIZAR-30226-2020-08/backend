7f6b30821222fbdb84cb5da5ad53a2abaed70442
Misael-Arauzo <75611395+Misael-Arauzo@users.noreply.github.com>
2021-04-14 01:05:26 +0200

    Pertenece_chat (REVISAR)

--- app/controllers/pertenece_chat.controller.js
+++ app/controllers/pertenece_chat.controller.js
@@ -21,83 +21,57 @@ exports.create = (req, res) => {
       });
   };
 
-exports.findAll = (req, res) => {
-    const pertence_chat = req.body.pertence_chat;
-    var condition = pertenece_chat ? { pertence_chat: { [Op.iLike]: `%${pertence_chat}%` } } : null;
-    PerteneceChat.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Error recuperando pertenece_chats."
-        });
-      });
-  };
-
 exports.find = (req, res) => {
-    const pertence_chat = req.body.pertence_chat;
-    PerteneceChat.findByPk(pertence_chat)
-    .then(data => {
-        res.send({data});
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: 
-                err.message || "Error recuperando pertenece_chat: " + pertence_chat
-        });
-    });
-  };
-
-exports.update = (req, res) => {
-    const pertence_chat = req.body.pertence_chat;
-    PerteneceChat.update(req.body, {
-      where: { pertence_chat: pertence_chat }
+  const usuario = req.body.usuario;
+  const idchat = req.body.idchat;
+  PerteneceChat.findAll({
+    where: { idchat: idchat, usuario:usuario }
   })
-  .then(num => {
-      if (num == 1) {
-          res.send({
-              message: "pertenece_chat actualizado."
-          });
-      } else {
-          res.send({
-              message: `No se puede actualizar pertenece_chat: ${pertence_chat}.`
-          });
-      }
+  .then(data => {
+      res.send({data});
   })
   .catch(err => {
       res.status(500).send({
           message: 
-              err.message || "Error actualizando pertenece_chat: " + pertence_chat
+              err.message || `Error recuperando pertenece_chat: ${idchat},${usuario}`
+      });
+  });
+};
+
+//no se va a usar, los dos clave primaria
+exports.update = (req, res) => {
+  const usuario = req.body.usuario;
+  PerteneceChat.update(req.body, {
+    where: { usuario: usuario }
+  })
+  .then(num => {
+          res.send({ message: "chat actualizado." });
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: 
+              err.message || `Error actualizando el chat ${usuario}`
       });
   });
 };
 
 exports.delete = (req, res) => {
-    const pertence_chat = req.body.pertence_chat;
-    PerteneceChat.destroy({
-      where: { pertence_chat: pertence_chat }
-    })
-    .then(num => {
-        if (num == 1) {
-            res.send({
-                status: "Eliminado"
-            });
-        } else {
-            res.send({
-                status:  `No se puede eliminar pertenece_chat: ${pertence_chat}.`
-            });
-        }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: 
-                err.message || "Error eliminando pertenece_chat " + pertence_chat
-        });
-    });
+  const usuario = req.body.usuario;
+  const idchat = req.body.idchat;
+  PerteneceChat.destroy({
+    where: { idchat: idchat, usuario:usuario }
+  })
+  .then(num => {
+          res.send({ status: "Eliminado" });
+  })
+  .catch(err => {
+      res.status(500).send({
+          message: err.message || `Error eliminando el chat ${idchat}, ${usuario}`
+      });
+  });
 };
 
+//no probamos por si acaso
 exports.deleteAll = (req, res) => {
   PerteneceChat.destroy({
     where: {},
@@ -110,6 +84,22 @@ exports.deleteAll = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Error eliminando pertenece_chats."
+      });
+    });
+};
+
+//Devuelve los usuarios pertenecientes al chat que se pasa
+exports.findAll = (req, res) => {
+  const idchat = req.body.idchat;
+  var condition = idchat ? { idchat: { [Op.iLike]: `${idchat}` } } : null;
+  PerteneceChat.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error recuperando pertenece_chats."
       });
     });
 };
