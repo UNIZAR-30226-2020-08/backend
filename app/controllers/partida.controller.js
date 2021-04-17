c0b5fdbb622942d7a015bf5f71943755e8cb2177
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-04-17 14:42:47 +0200

    Update partida.controller.js

--- app/controllers/partida.controller.js
+++ app/controllers/partida.controller.js
@@ -44,7 +44,7 @@ exports.iniciar_partida = (req, res) => {
 exports.findAll = (req, res) => {
   tipo = req.params.tipo;
   console.log(req.params);
-  Partida.findAndCountAll({ where: { tipo: tipo} })
+  Partida.findAll({ where: { tipo: tipo} })
     .then(dataPartidas => {
       //var partidasDisponibles = [];
       //partidasDisponibles = devolverPartidas(dataPartidas,tipo);
