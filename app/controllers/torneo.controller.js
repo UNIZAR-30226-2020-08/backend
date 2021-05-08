5a80e3b4ed413cdcc2112bf3903977a2ff581280
aaronainzon17 <aaronainzon@gmail.com>
2021-05-08 19:35:37 +0200

    matchhRound

--- app/controllers/torneo.controller.js
+++ app/controllers/torneo.controller.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const db = require("../models");
 const Torneo = db.torneo;
 const Participantes = db.participantes_torneo;
@@ -22,13 +23,13 @@ exports.create = async(req, res) => {
     if (dataTorneo.nparticipantes === 8){
       nPartidas = 7;
       partidas = ['1.1','1.2','1.3','1.4','2.1','2.2',
-                  '3']
+                  '3.1']
     }else if(dataTorneo.nparticipantes === 16){
       nPartidas = 15;
       partidas = ['0.1','0.2','0.3','0.4',
                   '0.5','0.6','0.7','0.8',
                   '1.1','1.2','1.3','1.4','2.1','2.2',
-                  '3']
+                  '3.1']
     }
     //console.log(partidas)
     var a;
@@ -57,32 +58,14 @@ exports.create = async(req, res) => {
       };
       await Partida.create(dataP)
       .then(async dataPartida => {
-        await Carta.findAll()
-        .then(async dataCartas => {
-          for (card of dataCartas)
-          {
-            const carta_disponible = {
-              partida: dataPartida.nombre,
-              carta: card.carta
-            };
-            const data = await CartaDisponible.create(carta_disponible)
-              console.log(`Se ha insertado el ${data.carta}`);
-          }
-          const dataCuad = {
-            id_torneo: dataTorneo.nombre,
-            id_partida: dataPartida.nombre,
-            fase: a,
-            eq_winner: 6
-          };
-          const dataCuadro = await Cuadro.create(dataCuad)
-          console.log(dataPartida,dataCuadro);
-        })
-        .catch(err => {
-          res.status(500).send({
-            message:
-              err.message || "Error recuperando cartas."
-          });
-        });
+        const dataCuad = {
+          id_torneo: dataTorneo.nombre,
+          id_partida: dataPartida.nombre,
+          fase: a,
+          eq_winner: 6
+        };
+        const dataCuadro = await Cuadro.create(dataCuad)
+        console.log(dataPartida,dataCuadro);
       })
       .catch(err => {
         res.status(500).send({
@@ -101,14 +84,10 @@ exports.create = async(req, res) => {
 exports.matchRound = async (req,res) => {
   try {
     const torneo = req.params.torneo
-    const ronda = req.params.ronda
-    const rondaAux = ronda - 1
-    const notFirstRound = await Cuadro.findAll({where: {id_torneo : torneo, fase: rondaAux}})
-    if (notFirstRound === undefined ){
-      //Implica que es la primera ronda
-    }else{
-      //Ha habido una ronda previa
-    }
+    const rondaIni = req.params.ronda
+    const rondaPrev = (rondaIni - 1).toString() + '.'
+    const ronda = rondaIni.toString() + '.'
+    
     const dataTorneo = await Torneo.findByPk(torneo)
     var maxEnTorneo 
     var maxEnPartida
@@ -125,47 +104,138 @@ exports.matchRound = async (req,res) => {
       maxEnTorneo = 32
       maxEnPartida = 4
     }
-    const dataPart = await Participantes.findAndCountAll({ where: { torneo: torneo } }) 
-    console.log(dataPart.count)
-    if (dataPart.count < maxEnTorneo){
-      res.status(500).send('El torneo no esta completo, NO SE PUEDEN EMPEZAR LOS EMPAREJAMIENTOS' )
-    }else{
-      // Mezclamos el array de juagdores para hacer los emparejamientos
-      const array  = (dataPart.rows).sort(() => Math.random() - 0.5)
-      const dataRonda = await Cuadro.findAll({
-                                where: {
-                                  id_torneo:{ [Op.eq]: `${dataTorneo.nombre}` },
-                                  fase: { [Op.like]: `%${ronda}%` }}
+    const dataRonda = await Cuadro.findAll({
+      where: {
+        id_torneo:{ [Op.eq]: `${dataTorneo.nombre}` },
+        fase: { [Op.like]: `%${ronda}%` }}
+      })
+    const prevRound = await Cuadro.findAll({
+                                  where: {id_torneo : torneo, 
+                                  fase: { [Op.like]: `%${rondaPrev}%` }}
                                 })
-      for (a of dataRonda){
-        for (var i = 0; i < maxEnPartida; i++){
-          const dataCount = await Pertenece.findAll({where: { partida: a.id_partida}})
-          if (dataTorneo.tipo === 0 && dataCount.length >= 2){
-            res.status(500).send("Partida individual llena");
-          }
-          else if (dataTorneo.tipo === 1 && dataCount.length >= 4){
-            res.status(500).send("Partida dobles llena");
-          }else{
-            var player = array.pop();
-            const data = 
-            {
-              jugador: player.jugador,
-              partida: a.id_partida,
-              equipo: (dataCount.length) % 2,
-              orden: dataCount.length + 1,
-              c1: req.body.c1 ? req.body.c1 : 'NO',
-              c2: req.body.c2 ? req.body.c2 : 'NO',
-              c3: req.body.c3 ? req.body.c3 : 'NO',
-              c4: req.body.c4 ? req.body.c4 : 'NO',
-              c5: req.body.c5 ? req.body.c5 : 'NO',
-              c6: req.body.c6 ? req.body.c6 : 'NO',  
+    if (prevRound.length === 0){
+      //Implica que es la primera ronda del torneo
+      const dataPart = await Participantes.findAndCountAll({ where: { torneo: torneo } }) 
+      //console.log(dataPart.count)
+      if (dataPart.count < maxEnTorneo){
+        res.status(500).send('El torneo no esta completo, NO SE PUEDEN EMPEZAR LOS EMPAREJAMIENTOS' )
+      }else{
+        // Mezclamos el array de juagdores para hacer los emparejamientos
+        const array  = (dataPart.rows).sort(() => Math.random() - 0.5)
+        for (a of dataRonda){
+          for (var i = 0; i < maxEnPartida; i++){
+            const dataCount = await Pertenece.findAll({where: { partida: a.id_partida}})
+            if (dataTorneo.tipo === 0 && dataCount.length >= 2){
+              res.status(500).send("Partida individual llena");
             }
-            const dataPer = await Pertenece.create(data)
+            else if (dataTorneo.tipo === 1 && dataCount.length >= 4){
+              res.status(500).send("Partida dobles llena");
+            }else{
+              var player = array.pop();
+              const data = 
+              {
+                jugador: player.jugador,
+                partida: a.id_partida,
+                equipo: (dataCount.length) % 2,
+                orden: dataCount.length + 1,
+                c1: req.body.c1 ? req.body.c1 : 'NO',
+                c2: req.body.c2 ? req.body.c2 : 'NO',
+                c3: req.body.c3 ? req.body.c3 : 'NO',
+                c4: req.body.c4 ? req.body.c4 : 'NO',
+                c5: req.body.c5 ? req.body.c5 : 'NO',
+                c6: req.body.c6 ? req.body.c6 : 'NO',  
+              }
+              const dataPer = await Pertenece.create(data)
+            }
+          }
+          //Se inicaliza la baraja de la partida
+          const dataCartas = await Carta.findAll()
+          for (card of dataCartas){
+            const carta_disponible = {
+              partida: a.id_partida,
+              carta: card.carta
+            };
+            const data = await CartaDisponible.create(carta_disponible)
+            console.log(`Se ha insertado el ${data.carta}`);
           }
         }
+        res.status(200).send('se ha emparejado correctamente la ronda')
       }
-      res.status(200).send('se ha emparejado correctamente la ronda')
+    }else{
+      //Ha habido una ronda previa clasificatoria previa 
+        var winners = []
+        for (a of prevRound){
+          const team = await Pertenece.findAll({where:{partida:a.id_partida, equipo: a.eq_winner}})
+          for (i of team){
+            const dataWin = {
+              nombre: i.jugador,
+              fase: a.fase,
+            }
+            await winners.push(dataWin)
+          }
+        }
+        //Ordena los resultados al reves para poder extraerlos con pop
+        winners.sort(function (a,b) {
+          if (a.fase < b.fase){
+            return 1;
+          } 
+          if (a.fase > b.fase) {
+            return -1;
+          }
+          // a must be equal to b
+          return 0;
+        })
+        for (a of dataRonda){
+          for (var i = 0; i < maxEnPartida; i++){
+            const dataCount = await Pertenece.findAll({where: { partida: a.id_partida}})
+            if (dataTorneo.tipo === 0 && dataCount.length >= 2){
+              res.status(500).send("Partida individual llena");
+            }
+            else if (dataTorneo.tipo === 1 && dataCount.length >= 4){
+              res.status(500).send("Partida dobles llena");
+            }else{
+              var player = winners.pop();
+              const data = 
+              {
+                jugador: player.nombre,
+                partida: a.id_partida,
+                equipo: (dataCount.length) % 2,
+                orden: dataCount.length + 1,
+                c1: req.body.c1 ? req.body.c1 : 'NO',
+                c2: req.body.c2 ? req.body.c2 : 'NO',
+                c3: req.body.c3 ? req.body.c3 : 'NO',
+                c4: req.body.c4 ? req.body.c4 : 'NO',
+                c5: req.body.c5 ? req.body.c5 : 'NO',
+                c6: req.body.c6 ? req.body.c6 : 'NO',  
+              }
+              console.log(data)
+              if (dataTorneo.tipo === 1 && i < 2){
+                console.log('entra al if')
+                data.equipo = 0
+                if(i === 0){data.orden = 1}else{data.orden = 3} 
+              }else if(dataTorneo.tipo === 1 && i >= 2){
+                data.equipo = 1
+                if(i === 2){data.orden = 2}else{data.orden = 4}
+              }
+              console.log(data)
+              const dataPer = await Pertenece.create(data)
+              console.log(dataPer)
+            }
+          }
+          //Se inicaliza la baraja de la partida
+          const dataCartas = await Carta.findAll()
+          for (card of dataCartas){
+            const carta_disponible = {
+              partida: a.id_partida,
+              carta: card.carta
+            };
+            const data = await CartaDisponible.create(carta_disponible)
+            console.log(`Se ha insertado el ${data.carta}`);
+          }
+        }
+      res.status(200).send(`Se ha realizado el emparejamiento de la ronda ${ronda}`)
     }
+    
   }catch(err){
     return res.status(500).send({ message: err | 
       'se ha producido un error haciendo los emparejamientos'});
