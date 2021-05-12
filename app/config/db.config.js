1275f782ff42014bf5c15afab88bb70a2013c67a
aaronainzon17 <aaronainzon@gmail.com>
2021-05-13 00:17:16 +0200

    prueba heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    HOST: "localhost", //148.3.18.81
+    HOST: "ec2-34-247-118-233.eu-west-1.compute.amazonaws.com", //148.3.18.81
     PORT: "5432",
-    USER: "las10ultimas",
-    PASSWORD: "roberta_williams63",
-    DB: "las10ultimas_bd",
+    USER: "vefjxjtwckqexq",
+    PASSWORD: "82637aa982638b9ef0079f26f7fe7c43af39b1c3bfb06ae5d25ba3b1083c8450",
+    DB: "dmo9h5pup9a6e",
     SCHEMA: "public",
     dialect: "postgres",
 };
