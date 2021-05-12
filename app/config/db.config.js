0b23b1d1735595bd24ea9e661dfde31283d13037
aaronainzon17 <aaronainzon@gmail.com>
2021-05-13 00:32:52 +0200

    p1

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,5 @@ module.exports = {
     PORT: "5432",
     USER: "vefjxjtwckqexq",
     PASSWORD: "82637aa982638b9ef0079f26f7fe7c43af39b1c3bfb06ae5d25ba3b1083c8450",
-    DB: "dmo9h5pup9a6e",
-    SCHEMA: "public",
-    dialect: "postgres",
+    DB: "dmo9h5pup9a6e"
 };
--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  schema: dbConfig.SCHEMA,
+  //schema: dbConfig.SCHEMA,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
   define:{
