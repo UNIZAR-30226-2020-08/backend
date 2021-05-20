59ae8b6a9ff7f24ea21fd1ff6227afec70dc343b
Aaron Ibañez Espes <aaronainzon@gmail.com>
2021-05-20 12:42:43 +0200

    regah

--- app/controllers/jugada.controller.js
+++ app/controllers/jugada.controller.js
@@ -180,11 +180,13 @@ exports.prevRoundWinnerIA = async (req, res) => {
           const cartaWinner = await Carta.findByPk(winnerCard.carta);
           //Comparo rankings
           if(cartaJugada.ranking < cartaWinner.ranking){
-            winnerCard = { jugador: o.jugador, carta: o.carta }
+            const dataJugador = await Jugada.findOne({ where: {partida: partida, carta: cartaJugada.carta, nronda:nronda} })
+            winnerCard = { jugador: dataJugador.jugador, carta: cartaJugada.carta }
           }
         // Si la que tiras es triunfo y la otra no
         }else if ((o.carta[1] === dataPartida.triunfo[1]) && (winnerCard.carta[1] !== dataPartida.triunfo[1])){
-          winnerCard = { jugador: o.jugador, carta: o.carta }
+          const dataJugador = await Jugada.findOne({ where: {partida: partida, carta: cartaJugada.carta, nronda:nronda} })
+          winnerCard = { jugador: dataJugador.jugador, carta: cartaJugada.carta }
         }
       }
       console.log('EL GANADOR DE LA RONDA ANTERIOR IA: ', winnerCard)
@@ -241,7 +243,8 @@ exports.getRoundWinner = async (req, res) => {
           }
         // Si la que tiras es triunfo y la otra no
         }else if ((o.carta[1] === dataPartida.triunfo[1]) && (winnerCard.carta[1] !== dataPartida.triunfo[1])){
-          winnerCard = { jugador: o.jugador, carta: o.carta }
+          const dataJugador = await Jugada.findOne({ where: {partida: partida, carta: cartaJugada.carta, nronda:nronda} })
+          winnerCard = { jugador: dataJugador.jugador, carta: cartaJugada.carta }
         }
         puntosMano += cartaJugada.puntuacion;
       }
